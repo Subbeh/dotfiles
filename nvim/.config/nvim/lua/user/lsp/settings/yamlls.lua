abf766a9bd1184e9687cffe1e046c98927e84d90
Steven <steven001@gmail.com>
2023-03-28 12:09:49 +1100

    nvim

---
+++ nvim/.config/nvim/lua/user/lsp/settings/yamlls.lua
@@ -0,0 +1,9 @@
+return {
+  settings = {
+    yaml = {
+      schemas = {
+        ["https://raw.githubusercontent.com/ansible/ansible-lint/main/src/ansiblelint/schemas/ansible.json"] = "/*",
+      }
+    }
+  }
+}
