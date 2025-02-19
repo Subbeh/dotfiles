192e464c62cb00245b1a670ee519c0fd2da1769e
Steven Terwindt <git@sbbh.cloud>
2025-02-19 13:51:27 +1100

    zsh: enable autocomplete for infra tools

--- infra/.config/zsh/.zshrc.d/99-infra.zsh
+++ infra/.config/zsh/.zshrc.d/99-infra.zsh
@@ -1,3 +1,5 @@
 #!/bin/zsh
 
-complete -o nospace -C $(which terraform) terraform
+_chkcmd terraform && complete -o nospace -C $(which terraform) terraform
+_chkcmd aws && complete -C $(which aws_completer) aws
+_chkcmd aws-sso-util && eval "$(_AWS_SSO_UTIL_COMPLETE=zsh_source aws-sso-util)"
