4ba082850c7a03cef76f588437f81323a7de1de4
Steven Terwindt <git@sbbh.cloud>
2025-03-21 06:19:01 +1100

    hyprland & infra aliases

--- infra/.config/zsh/.zshrc.d/99-infra.zsh
+++ infra/.config/zsh/.zshrc.d/99-infra.zsh
@@ -4,19 +4,24 @@ _chkcmd terraform && complete -o nospace -C $(which terraform) terraform
 _chkcmd aws && complete -C $(which aws_completer) aws
 _chkcmd aws-sso-util && eval "$(_AWS_SSO_UTIL_COMPLETE=zsh_source aws-sso-util)"
 
-## ansible-playbook
+# ansible
 play() {
   pushd "${HOMELAB_DIR:?not set}/ansible"
   ansible-playbook "playbooks/$@"
   popd
 }
 
-# Completion function for ap command
+# #Completion function for ap command
 _play() {
   local playbooks
   playbooks=("${HOMELAB_DIR}/ansible/playbooks/"*(N:t))
   _describe 'playbooks' playbooks
 }
 
-# Register the completion function
+## Register the completion function
 compdef _play play
+
+# terraform
+alias tinit="terraform init"
+alias tplan="terraform plan"
+alias tapply="terraform apply"
