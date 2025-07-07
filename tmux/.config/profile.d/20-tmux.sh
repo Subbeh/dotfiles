cb40c04c800bc5875900cf839b6fc91ed3bef6b6
Steven Terwindt <git@sbbh.cloud>
2025-07-08 00:24:13 +1000

    tmux: add session persistence & tpad config

--- tmux/.config/profile.d/20-tmux.sh
+++ tmux/.config/profile.d/20-tmux.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#
+# Ensure user linger is enabled for tmux persistence
+if ! loginctl show-user "$USER" | grep -q "Linger=yes"; then
+  echo "Enabling user linger for tmux persistence..."
+  loginctl enable-linger "$USER"
+fi
 
 alias mux=__tmuxinator_sessions
 
