c340956d23249d465a916e2d6b4bed771ddafd2e
Steven Terwindt <git@sbbh.cloud>
2025-07-09 13:29:19 +1000

    ssh & gnupg: various changes

--- gnupg/.config/profile.d/20-gnupg.sh
+++ gnupg/.config/profile.d/20-gnupg.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 GNUPGHOME="${XDG_CONFIG_HOME:-${HOME}/.config}/gnupg"
-GPG_TTY=$(tty)
-GPG_AGENT_SOCK="$(gpgconf --list-dirs agent-socket)"
-export GNUPGHOME GPG_TTY GPG_AGENT_SOCK
+if test -t 0; then
+  GPG_TTY=$(tty)
+  GPG_AGENT_SOCK="$(gpgconf --list-dirs agent-socket)"
+  export GNUPGHOME GPG_TTY GPG_AGENT_SOCK
 
-unset SSH_AGENT_PID
-if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-	SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
-	export SSH_AUTH_SOCK
+  unset SSH_AGENT_PID
+  if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+    SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+    export SSH_AUTH_SOCK
+  fi
+
+  gpg-connect-agent -q updatestartuptty /bye >/dev/null
 fi
-
-gpg-connect-agent -q updatestartuptty /bye >/dev/null
 
 alias gpgtest="echo test | gpg --clearsign"
 alias gpgkill="gpgconf --kill gpg-agent"
