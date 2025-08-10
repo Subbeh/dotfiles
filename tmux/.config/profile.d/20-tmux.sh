9ec7b4b6d6521e1b25fd1aea3cb44a40adda1a3e
Steven Terwindt <git@sbbh.cloud>
2025-08-11 09:04:07 +1000

    tmux: add cda utility

--- tmux/.config/profile.d/20-tmux.sh
+++ tmux/.config/profile.d/20-tmux.sh
@@ -7,20 +7,3 @@ if _chkcmd loginctl && ! loginctl show-user "$USER" | grep -q "Linger=yes"; then
 fi
 
 alias mux=__tmuxinator_sessions
-
-cda() {
-  # cda - Change Directory All
-  current_dir=$(pwd)
-
-  window_index=$(tmux display-message -p '#I')
-
-  tmux list-panes -F '#{pane_index} #{pane_current_command}' | while read -r pane_index pane_command; do
-    if [ "$pane_index" != "$(tmux display-message -p '#{pane_index}')" ]; then
-      if [[ "$pane_command" =~ (bash|zsh|sh|fish) ]]; then
-        tmux send-keys -t "$window_index.$pane_index" "cd \"$current_dir\"" C-m
-      else
-        echo "Skipping pane $pane_index (running $pane_command)"
-      fi
-    fi
-  done
-}
