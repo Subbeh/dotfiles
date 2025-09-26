68a6dc8cd739ee13c7bf5da08007d5494a61fc16
Steven Terwindt <git@sbbh.cloud>
2025-09-26 14:16:18 +1000

    various: cleaning up some stuff

--- ssh/.config/profile.d/20-ssh.sh
+++ ssh/.config/profile.d/20-ssh.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 alias sshp='ssh -o PreferredAuthentications=password -o PubkeyAuthentication=no'
+alias scpp='scp -o PreferredAuthentications=password -o PubkeyAuthentication=no'
 alias sshc='ssh -F /dev/null'
