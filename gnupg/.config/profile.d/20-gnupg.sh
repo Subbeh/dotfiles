b3ea6e7e72d0ea690f14ad5700167cb62a76d4b4
Steven Terwindt <git@sbbh.cloud>
2026-07-11 10:23:58 +1000

    gnupg

---
+++ gnupg/.config/profile.d/20-gnupg.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+export GNUPGHOME="${XDG_CONFIG_HOME:-${HOME}/.config}/gnupg"
+
+if test -t 0; then
+  GPG_TTY=$(tty)
+  GPG_AGENT_SOCK="$(gpgconf --list-dirs agent-socket)"
+  export GPG_TTY GPG_AGENT_SOCK
+
+  unset SSH_AGENT_PID
+  if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
+    SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
+    export SSH_AUTH_SOCK
+  fi
+
+  gpg-connect-agent -q updatestartuptty /bye >/dev/null
+fi
+
+alias gpgtest="echo test | gpg --clearsign"
+alias gpgkill="gpgconf --kill gpg-agent"
+alias gpgrestart="gpgconf --kill gpg-agent && gpg-connect-agent /bye"
+alias gpgreload="gpg-connect-agent reloadagent /bye"
