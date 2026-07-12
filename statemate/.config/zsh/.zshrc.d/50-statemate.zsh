846024ce58bddd243b09d47bcb30e65e47cb3518
Steven Terwindt <git@sbbh.cloud>
2026-07-12 15:52:40 +1000

    various

--- statemate/.config/zsh/.zshrc.d/50-statemate.zsh
+++ statemate/.config/zsh/.zshrc.d/50-statemate.zsh
@@ -1,11 +1,12 @@
 #!/bin/zsh
 
 if [ "$PROFILE_OS" = "darwin" ]; then
-  _bin=${PROJECT_DIR}/statemate/dist/mate-darwin-arm64
+  _bin=${XDG_PROJECTS_HOME}/statemate/dist/mate-darwin-arm64
 elif [ "$PROFILE_OS" = "linux" ]; then
-  _bin=${PROJECT_DIR}/statemate/dist/mate-linux-amd64
+  _bin=${XDG_PROJECTS_HOME}/statemate/dist/mate-linux-amd64
 fi
 
-source <($_bin completion zsh)
-
-ln -s "$_bin" "$XDG_BIN_HOME/mate" 2>/dev/null
+if [ -x "$_bin" ]; then
+  source <($_bin completion zsh)
+  ln -s "$_bin" "$XDG_BIN_HOME/mate" 2>/dev/null
+fi
