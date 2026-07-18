6940f0a8e19eca8be2f6a37ea8b8f7a3373d3d82
Steven Terwindt <git@sbbh.cloud>
2026-07-18 14:39:33 +1000

    testing

--- hyprland/.config/hypr/config/monitors.lua
+++ hyprland/.config/hypr/config/monitors.lua
@@ -1,15 +1,40 @@
 local M = {}
 
-local function get_profile()
+local function is_connected(monitor, live)
+  Utils.debug("TEST 1: " .. #live)
+  for _, mon in ipairs(live) do
+    Utils.debug("TEST 2: " .. mon.description)
+    Utils.debug("TEST 3: " .. monitor)
+    -- Utils.debug("CHECKING: " .. mon.description .. " == " .. monitor.desc)
+    -- if mon.description == monitor.desc then
+    --   return true
+    -- end
+  end
+  return false
+end
+
+local function match_profile(live)
   for _, profile in ipairs(PROFILES) do
     for monitor in pairs(profile.monitors) do
-      Utils.debug("MON: " .. monitor)
+      if is_connected(monitor, live) then
+        Utils.debug("CONNECTED:" .. monitor.desc)
+      end
     end
   end
 end
 
+local function reload_layout()
+  local live = hl.get_monitors()
+  if #live > 0 then
+    match_profile(live)
+  end
+end
+
 M.init = function()
-  get_profile()
+  hl.timer(function()
+    reload_layout()
+    hl.on("monitor.added", reload_layout)
+  end, { timeout = 500, type = "oneshot" })
 end
 
 return M
