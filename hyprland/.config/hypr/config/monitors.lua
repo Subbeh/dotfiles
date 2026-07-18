1a335f9220fdf1a24d71600a7cd94e60703230ca
Steven Terwindt <git@sbbh.cloud>
2026-07-18 16:21:30 +1000

    test

--- hyprland/.config/hypr/config/monitors.lua
+++ hyprland/.config/hypr/config/monitors.lua
@@ -26,12 +26,29 @@ local function get_profile(live)
   end
 end
 
-local function apply_profile(profile)
+local function get_monitor(desc)
+  for name, item in pairs(MONITORS) do
+    if item.desc == desc then
+      return name
+    end
+  end
+end
+
+local function set_monitor(profile, monitor)
+  local mon_name = profile.monitors[get_monitor(monitor.description)]
+  if mon_name then
+    Utils.debug("MATCH: " .. monitor.description)
+  else
+    Utils.debug("NO MATCH: " .. monitor.description)
+  end
+end
+
+local function apply_profile(profile, live)
   for _, p in ipairs(PROFILES) do
     if p.name == profile then
       Utils.debug("APPLYING: " .. p.name)
-      for _, mon in ipairs(p.monitors) do
-        Utils.debug("TEST")
+      for _, mon in ipairs(live) do
+        set_monitor(p, mon)
       end
     end
   end
@@ -42,7 +59,7 @@ local function reload_layout()
   if #live > 0 then
     local profile = get_profile(live)
     if profile then
-      apply_profile(profile)
+      apply_profile(profile, live)
     end
   end
 end
