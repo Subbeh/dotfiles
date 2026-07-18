154ae538caf6bd7c80d2c46dddff0180e54b5c99
Steven Terwindt <git@sbbh.cloud>
2026-07-18 13:23:09 +1000

    testing

--- hyprland/.config/hypr/config/monitors.lua
+++ hyprland/.config/hypr/config/monitors.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-M.get_profile = function()
+local function get_profile()
   for _, profile in ipairs(PROFILES) do
     for monitor in pairs(profile.monitors) do
       Utils.debug("MON: " .. monitor)
