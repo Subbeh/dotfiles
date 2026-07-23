82202ff67d76f16ba0e48068c0d5c6e9d4f8c5a2
Steven Terwindt <git@sbbh.cloud>
2026-07-23 19:14:49 +1000

    bw stuff

--- statemate/.config/zsh/.zshrc.d/50-statemate.zsh
+++ statemate/.config/zsh/.zshrc.d/50-statemate.zsh
@@ -10,3 +10,12 @@ if [ -x "$_bin" ]; then
   source <($_bin completion zsh)
   ln -s "$_bin" "$XDG_BIN_HOME/mate" 2>/dev/null
 fi
+
+mate() {
+  command mate "$@"
+  local ret=$?
+  if [[ $ret -eq 0 && "$1" == "apply" ]]; then
+    pkill -u "$USER" zsh --signal=USR1 2>/dev/null || true
+  fi
+  return $ret
+}
